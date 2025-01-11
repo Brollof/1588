5b7c99bd8779fcecc7dd8dda85f8eabfd037c152
Brollof <thefish3r@gmail.com>
2025-01-11 22:28:10 +0100

    added p and f args; added checking with interval

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import json
+import time
 import base64
 import logging
+import argparse
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 from email.mime.text import MIMEText
@@ -20,6 +23,20 @@ LOG_FILEPATH = get_fullpath("bot.log")
 CREDENTIALS_FILEPATH = get_fullpath("credentials.json")
 TOKEN_FILEPATH = get_fullpath("token.json")
 SETTINGS_FILEPATH = get_fullpath("settings.json")
+URL = "https://wrzeszcz.1588.pl/takeaway/online-menu/"
+DEFAULT_CHECK_PERIOD_H = 1
+
+OPEN_HOURS = {
+    0: (11, 20), # Monday
+    1: (11, 20),
+    2: (11, 20),
+    3: (11, 20),
+    4: (11, 20),
+    5: (12, 21),
+    6: (12, 21)  # Sunday
+}
+
+sleep_hours = lambda h: time.sleep(h * 3600)
 
 
 def send_email(to: str, message: str):
@@ -53,14 +70,22 @@ def send_email(to: str, message: str):
         logging.error(f"An error occurred: {error}")
 
 
-def prepare_message(raport_data: dict[str, str]):
+def prepare_message(report_data: dict[str, str]):
     msg = ""
-    for menu_item_name, availability in raport_data.items():
+    for menu_item_name, availability in report_data.items():
         msg += f"{menu_item_name}: {availability}\n"
     return msg
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--period", type=int, default=DEFAULT_CHECK_PERIOD_H, help="Check period (hours)")
+    parser.add_argument("-f", "--force", action="store_true", default=False, help="Send report even when everything is sold out")
+    args = parser.parse_args()
+
+    period_h = args.period
+    force_send = args.force
+
     log_handlers = [logging.FileHandler(LOG_FILEPATH)]
     log_handlers.append(logging.StreamHandler())
 
@@ -71,6 +96,7 @@ def main():
         handlers=log_handlers)
 
     logging.info("Pierogi bot started")
+    logging.info(f"Checking every {period_h} hour(s)")
 
     try:
         with open(SETTINGS_FILEPATH, encoding="utf-8") as file:
@@ -83,44 +109,56 @@ def main():
         logging.critical(e)
         return
 
-    URL = "https://wrzeszcz.1588.pl/takeaway/online-menu/"
+    while True:
+        now = datetime.now()
+        open_today, close_today = OPEN_HOURS[now.weekday()]
+        if (open_today <= now.hour < close_today - 1) or force_send:
+            try:
+                response = requests.get(URL)
+            except Exception as e:
+                logging.critical(f"Bad URL: '{URL}'")
+                logging.critical(e)
+                return
 
-    try:
-        response = requests.get(URL)
-    except Exception as e:
-        logging.critical(f"Bad URL: '{URL}'")
-        logging.critical(e)
-        return
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+            raw_data = soup.find(id="__NEXT_DATA__")
+            data = json.loads(raw_data.contents[0])
+            menu_items = data["props"]["app"]["menu"]
 
-    raw_data = soup.find(id="__NEXT_DATA__")
-    data = json.loads(raw_data.contents[0])
-    menu_items = data["props"]["app"]["menu"]
+            report_data = {}
 
-    raport_data = {}
+            send_mail = False
+            for item in menu_items:
+                found = False
+                for check in checklist:
+                    item_name = item["name"]
+                    if check in item_name.lower():
+                        logging.debug(f"Item '{item_name}': {item["attributes"]}")
+                        if 'SOLD_OUT' not in item["attributes"]:
+                            send_mail = True
+                            logging.info("Item found, email will be send")
+                            report_data[item_name] = "Available"
+                        else:
+                            report_data[item_name] = "Sold out"
+                    found = True
+                if not found:
+                    report_data[check] = "Not found!"
 
-    send_mail = False
-    for item in menu_items:
-        found = False
-        for check in checklist:
-            item_name = item["name"]
-            if check in item_name.lower():
-                logging.debug(f"Item '{item_name}': {item["attributes"]}")
-                if 'SOLD_OUT' not in item["attributes"]:
-                    send_mail = True
-                    logging.info("Item found, email will be send")
-                    raport_data[item_name] = "Available"
-                else:
-                    raport_data[item_name] = "Sold out"
-            found = True
-        if not found:
-            raport_data[check] = "Not found!"
+            if send_mail or force_send:
+                msg = prepare_message(report_data)
+                logging.info(f"Sending message:\n{msg}")
+                # send_email(recipient, msg)
+            else:
+                logging.info("Not sending email - everything is sold out")
 
-    msg = prepare_message(raport_data)
-    logging.info(f"Sending message:\n{msg}")
+            open_today, close_today = OPEN_HOURS[datetime.now().weekday()]
+            now = datetime.now()
+        else:
+            logging.info(f"It's closed, there's no point in checking the menu")
 
-    send_email(recipient, msg)
+        logging.info(f"Going to sleep for {period_h} hour(s)...")
+        sleep_hours(period_h)
 
 
 if __name__ == '__main__':
